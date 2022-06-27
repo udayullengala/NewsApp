83cfdc32d50e57a363f0059cc11c62c8bb008a08
Uday Ullengala <u.ullengala@gmail.com>
2022-06-27 21:25:16 +0530

    Calling API

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Search = () => {
-    return(
-        <>
-            <h3>Search</h3>
-        </>
-    )
+
+    let API = "http://hn.algolia.com/api/v1/search?query=React"
+    const [isLoading, setIsLoading] = useState(true)
+    const Call_data = async (url) => {
+        let call = await fetch(url)
+        let data = await call.json()
+        setIsLoading(false)
+        console.log(data)
+    }
+    useEffect(() => {
+        Call_data(API) 
+    }, [])
+
+    if(isLoading){
+        return <h3>Loading...</h3>
+    }
+    else{
+        return(
+            <>
+                <h3>Search</h3>
+            </>
+        )
+    }
+
+   
 }
 
 export default Search;
